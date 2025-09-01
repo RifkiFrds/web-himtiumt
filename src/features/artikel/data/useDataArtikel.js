d5f8e084896cc82559a4c7cbf4c3f70eae36cae2
muhamadrifkifirdaus22@gmail.com <muhamadrifkifirdaus22@gmail.com>
2025-09-01 20:43:05 +0700

    update data artikel

---
+++ src/features/artikel/data/useDataArtikel.js
@@ -0,0 +1,12 @@
+export const dataArtikel = [
+    {
+        id: 1,
+        title: "Lomba",
+        description: "loremp ipsum",
+        images: "image/..",
+        time: "23 Januari, 2025",
+    },
+    {
+        
+    }
+]
