0778ee34b0873370f8c932f28b65991093a62298
muhamadrifkifirdaus22@gmail.com <muhamadrifkifirdaus22@gmail.com>
2025-09-10 21:42:31 +0700

    update Artikel Page

--- src/features/artikel/view/ArtikelPage.jsx
+++ src/features/artikel/view/ArtikelPage.jsx
@@ -1,15 +1,9 @@
 import ArtikelSection from "../components/ArtikelSection";
-import ComingSoonPage from "../../display/view/ComingSoonPage";
 
 export default function ArtikelPage() {
   return (
     <>
-    <ArtikelSection />
-    <div className="bg-white relative">
-        <ComingSoonPage />
-    </div>
-    
+      <ArtikelSection />
     </>
-  )
+  );
 }
-
