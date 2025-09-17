2fa20e833da6f42df32eee454dea2c2ef27e8989
muhamadrifkifirdaus22@gmail.com <muhamadrifkifirdaus22@gmail.com>
2025-09-17 22:56:47 +0700

    fix bug

--- src/features/tutorial/components/HeroSection.jsx
+++ src/features/tutorial/components/HeroSection.jsx
@@ -12,13 +12,10 @@ export default function HeroSection({ onSearchChange, onSearchSubmit }) {
   ];
 
   return (
-    <section className="pt-20 pb-10 relative">
+    <section className="pt-20 pb-10 relative animate-fade-in-up">
       <div className="max-w-7xl mx-auto px-4 text-center">
-        <h2 className="text-4xl md:text-5xl font-bold text-gray-900">
-          Belajar Asik di Ruang Tutorial <span className="block w-full py-2 text-transparent bg-clip-text leading-[3rem] bg-gradient-to-r from-yellow-400 to-blue-800 text-center lg:inline">
-            HIMTI
-            </span>
-        </h2>
+       <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-gray-900 leading-tight"> Belajar Asik di Tutorial{" "} <span className="inline-block text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 to-blue-800"> HIMTI </span>
+    </h2>
         <Poppins className="mt-4 max-w-4xl mx-auto text-lg text-gray-600">
           Mau jago teknologi? Di sini tempatnya! Temukan tips, trik, dan tutorial terkini yang gampang dipahami. Yuk, mulai jelajahi sekarang.
         </Poppins>
