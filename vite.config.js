4847d538da6a0b762281335ad6cfdbb7bc1dcc24
muhamadrifkifirdaus22@gmail.com <muhamadrifkifirdaus22@gmail.com>
2025-09-18 22:08:48 +0700

    test

--- vite.config.js
+++ vite.config.js
@@ -6,3 +6,26 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 });
+
+
+// FOR DEVELOPMENT MODE
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'https://himti-api.vercel.app',
+
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, ''),
+//       },
+//     },
+//   },
+// });
