2eafbe2d5d761251081d07ecb58c115e496ac7db
muhamadrifkifirdaus22@gmail.com <muhamadrifkifirdaus22@gmail.com>
2025-09-18 21:57:34 +0700

    fetch API

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,1 +1,15 @@
-console.log("test")
+import axios from 'axios';
+
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const HEADER_KEY = import.meta.env.VITE_API_HEADER_KEY;
+const SECRET_KEY = import.meta.env.VITE_API_SECRET_KEY;
+
+const apiClient = axios.create({
+  baseURL: BASE_URL, 
+  headers: {
+    'Content-Type': 'application/json',
+    [HEADER_KEY]: SECRET_KEY,
+  },
+});
+
+export default apiClient;
