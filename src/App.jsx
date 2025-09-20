85ef232d903c32d438ff3d12d478e3238e035623
muhamadrifkifirdaus22@gmail.com <muhamadrifkifirdaus22@gmail.com>
2025-09-20 12:12:18 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router } from "react-router";
 import AppRoutes from "./routes";
 import ScrollToTop from "./lib/ScrollToTop";
+import ScrollToHasElement from "./lib/ScrollToHashElement";
 
 function App() {
   return (
     <Router>
       <ScrollToTop />
+      <ScrollToHasElement />
       <AppRoutes />
     </Router>
   );
