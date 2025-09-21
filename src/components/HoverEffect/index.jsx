8202660c1f9bf32bec08ebabd56b7e8433510111
muhamadrifkifirdaus22@gmail.com <muhamadrifkifirdaus22@gmail.com>
2025-09-21 14:38:19 +0700

    add comonents HoverEffect

---
+++ src/components/HoverEffect/index.jsx
@@ -0,0 +1,46 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { cn } from "../../lib/utils";
+
+export const HoverEffect = ({ items, className, children }) => {
+  let [hoveredIndex, setHoveredIndex] = useState(null);
+
+  return (
+    <div
+      className={cn(
+        "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3",
+        className
+      )}
+    >
+      {items.map((item, idx) => (
+        <div
+          key={item?.id}
+          className="relative group block p-2 h-full w-full"
+          onMouseEnter={() => setHoveredIndex(idx)}
+          onMouseLeave={() => setHoveredIndex(null)}
+        >
+          <AnimatePresence>
+            {hoveredIndex === idx && (
+              <motion.span
+                className="absolute inset-0 h-full w-full bg-gradient-to-br from-yellow-400/80 to-blue-600/80 block rounded-3xl"
+                layoutId="hoverBackground"
+                initial={{ opacity: 0 }}
+                animate={{
+                  opacity: 1,
+                  transition: { duration: 0.15 },
+                }}
+                exit={{
+                  opacity: 0,
+                  transition: { duration: 0.15, delay: 0.2 },
+                }}
+              />
+            )}
+          </AnimatePresence>
+          {/* Render children sebagai fungsi untuk melewatkan item */}
+          {children(item)}
+        </div>
+      ))}
+    </div>
+  );
+};
