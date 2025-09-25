68bd87fbde007304e4cf5320bfc27e743d28cbd8
muhamadrifkifirdaus22@gmail.com <muhamadrifkifirdaus22@gmail.com>
2025-09-25 20:12:08 +0700

    optimizing

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,30 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import viteImagemin from "vite-plugin-imagemin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteImagemin({
+      gifsicle: { optimizationLevel: 7, interlaced: false },
+      optipng: { optimizationLevel: 7 },
+      mozjpeg: { quality: 75 }, // kompres JPG
+      pngquant: { quality: [0.7, 0.9], speed: 4 }, // kompres PNG
+      svgo: { plugins: [{ removeViewBox: false }] },
+    }),
+  ],
+  build: {
+    chunkSizeWarningLimit: 2000, // default 500 KB
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"], 
+          lottie: ["lottie-web"],
+        },
+      },
+    },
+  },
 });
-
-
